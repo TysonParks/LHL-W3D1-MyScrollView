f9c16cd079809da6f59bb3d959bfc53a8021bb91
tysonparks <typarks@gmail.com>
2018-02-26 12:09:42 -0800

    Connected Main View to the ViewController with an IBOutlet

--- MyScrollView/ViewController.m
+++ MyScrollView/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *mainView;
 
 @end
 
@@ -19,11 +20,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    
+    
+    
 }
-
 
 @end
